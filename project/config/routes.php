060c06e22179e540e70c46ed2b5d1f7d0767d56f
Peter Lockett <cerwym@googlemail.com>
2018-09-08 18:05:20 +0100

    Get All Games route now uses Query builder in container

--- project/config/routes.php
+++ project/config/routes.php
@@ -3,6 +3,11 @@
 // Get all consoles.
 // POST Console
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+use Illuminate\Database\Connection;
+
 $app->post('consoles/', function ($request) {
 
     echo "POST - Adding a console, I don't do anything just yet.";
@@ -17,13 +22,17 @@ $app->get('/consoles', function () {
 
 $app->post('/games', function ($request) {
 
-    echo "POST - don't do anything just yet.";
+    echo "POST -> Create new game, i dont do anything yet.";
 
 });
 
-$app->get('/games', function () {
+$app->get('/games', function (Request $request, Response $response) {
 
-    echo "GET - Get all games, dont do anything just yet.";
+    $db = $this->get('db');
+
+    $rows = $db->table('t_game')->get();
+
+    return $response->withJson($rows);
 });
 
 $app->get('/games?console={console_id}', function ($request) {
