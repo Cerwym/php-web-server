9a6639cc874de49d2bec98f5c73997f43ac0adc1
Peter Lockett <cerwym@googlemail.com>
2018-09-10 23:29:14 +0100

    Further Unit Tests added for GameController
    Get All and Delete Game tests added.

--- project/tests/ControllerTests/GameControllerTests.php
+++ project/tests/ControllerTests/GameControllerTests.php
@@ -14,7 +14,6 @@ class GameControllerTests extends BaseTest {
 
     /** @test */
     public function create_game_when_data_isValid_returns_201created() {
-
         $payload = [
             'game_name' => 'Super Mario Brothers 3',
             'console_code' => 'NES'
@@ -53,7 +52,6 @@ class GameControllerTests extends BaseTest {
 
     /** @test */
     public function get_game_by_id_isValid_returns_data() {
-
         $response = $this->request('GET', '/api/games/1');
         $body = json_decode((string)$response->getBody(), true);
 
@@ -65,9 +63,42 @@ class GameControllerTests extends BaseTest {
         $this->assertEquals('SNES', $body['console_id']);
     }
 
+    /** @test */
     public function get_game_by_id_isInvalid_returns_404() {
-
         $response = $this->request('GET', '/api/games/9999999');
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    /** @test */
+    public function get_all_returns_200() {
+        $response = $this->request('GET', '/api/games');
+
+        $body = json_decode((string)$response->getBody(), true);
+        $this->assertNotEmpty($body);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    /** @test */
+    public function delete_game_by_valid_id_returns_202() {
+        // In order to correctly test that the delete is working, we should create a record THEN delete the id returned from create.
+        $payload = [
+            'game_name' => 'Super Mario 64',
+            'console_code' => 'N64'
+        ];
+
+        $createResponse = $this->request('POST', '/api/games', $payload);
+
+        $body = json_decode((string)$createResponse->getBody(), true);
+        $idToDelete = $body['game_id'];
+        $deleteResponse = $this->request('DELETE', "/api/games/$idToDelete");
+
+        $this->assertEquals(202, $deleteResponse->getStatusCode());
+    }
+
+    /** @test */
+    public function delete_game_by_invalid_id_returns_404() {
+        $response = $this->request('DELETE', '/api/games/99999');
+
         $this->assertEquals(404, $response->getStatusCode());
     }
 }
